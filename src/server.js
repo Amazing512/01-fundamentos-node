d31afed74ab63e83dee522226ed92faa743eae0a
Matheus Mastelini <matheusmastelini@gmail.com>
2023-04-09 22:47:01 -0300

    update: Finishing project

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 // GET => Buscar um recurso do back-end
 // POST => Criar um recurso no back-end
@@ -15,25 +18,36 @@ import http from "node:http";
 
 // HTTP Status Code
 
-const users = [];
+// Query Parameters: URL Stateful => Filtros, paginação, não obrigatórios
+// Ex.: http://localhost:3333/users?userId=1&name=Diego
 
-const server = http.createServer((req, res) => {
+// Route Parameters: Identificação de recurso =>
+// Ex.: GET http://localhost:3333/users/1
+// Ex.: DELETE http://localhost:3333/users/1
+
+// Request Body: Envio de informações de um formulário (HTTPS)
+// Ex.: POST http://localhost:3333/users
+
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
-  }
-  if (method === "POST" && url === "/users") {
-    users.push({
-      id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
-    });
+  await json(req, res);
 
-    return res.writeHead(201).end();
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
+
+  if (route) {
+    const routeParams = req.url.match(route.path);
+
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
+    return route.handler(req, res);
   }
+
   return res.writeHead(404).end();
 });
 
